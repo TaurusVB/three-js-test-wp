f26aec8b109183926f99361c6aaf7e829794810c
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-12-28 12:35:05 +0200

    Added dat gui controller for sphere speed wireframe color

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import * as dat from "dat.gui";
 
 const renderer = new THREE.WebGLRenderer();
 
@@ -52,12 +53,38 @@ const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 sphere.position.set(-10, 10, 10);
 scene.add(sphere);
 
+const gui = new dat.GUI();
+
+const options = {
+  sphereColor: "#ffea00",
+  wireframe: false,
+  speed: 0.01,
+};
+
+gui.addColor(options, "sphereColor").onChange(changeSphereColor);
+
+gui.add(options, "wireframe").onChange(changeWireframe);
+
+gui.add(options, "speed", 0, 0.1);
+
+let step = 0;
+
 function animate(time) {
   box.rotation.x = time / 1000;
   box.rotation.y = time / 1000;
-  box.rotation.z = time / 1000;
+
+  step += options.speed;
+  sphere.position.y = 10 * Math.abs(Math.sin(step));
 
   renderer.render(scene, camera);
 }
 
 renderer.setAnimationLoop(animate);
+
+function changeSphereColor(e) {
+  sphere.material.color.set(e);
+}
+
+function changeWireframe(e) {
+  sphere.material.wireframe = e;
+}
